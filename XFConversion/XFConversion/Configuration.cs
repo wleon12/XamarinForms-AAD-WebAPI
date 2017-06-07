c685fb0aeeb911e23d14db74df4274e901fc2ca2
Washington Leon-Jordan <wleonjordan@gmail.com>
2017-06-07 15:06:21 -0400

    uncommented some code

--- XFConversion/XFConversion/Configuration.cs
+++ XFConversion/XFConversion/Configuration.cs
@@ -8,12 +8,12 @@ namespace XFConversion
 {
     public class Configuration
     {
-        //public const string ClientId = "6a26732c-369b-4609-a0d8-b650662deeb1"; // Put your mobile app ClientId
-        //public const string Authority = "https://login.windows.net/wleonjordangmail.onmicrosoft.com/"; // Default authority for Azure AD
-        //public const string Resource = "https://canadawebapi.azurewebsites.net"; // Put your API ID URI 
-        //public const string RedirectUri = "https://mycanadalcient"; // Put your mobile app Redirect Uri (declared in Azure AD Apps)
-        //public const string ApiUri = "https://canadawebapi.azurewebsites.net/api/"; // Put you API actual URL
-        //public const string Proxy = "http://10.198.255.114:8888/"; // Proxy for fiddler 
+        public const string ClientId = "6a26732c-369b-4609-a0d8-b650662deeb1"; // Put your mobile app ClientId
+        public const string Authority = "https://login.windows.net/wleonjordangmail.onmicrosoft.com/"; // Default authority for Azure AD
+        public const string Resource = "https://canadawebapi.azurewebsites.net"; // Put your API ID URI 
+        public const string RedirectUri = "https://mycanadalcient"; // Put your mobile app Redirect Uri (declared in Azure AD Apps)
+        public const string ApiUri = "https://canadawebapi.azurewebsites.net/api/"; // Put you API actual URL
+        public const string Proxy = "http://10.198.255.114:8888/"; // Proxy for fiddler 
 
 
     }
